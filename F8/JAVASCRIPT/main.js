835c5cacc2822fbb57fba9fd659969b84d30d92a
chaly <mila.tran87@gmail.com>
2025-03-24 01:36:56 +0700

    fake API

--- F8/JAVASCRIPT/main.js
+++ F8/JAVASCRIPT/main.js
@@ -1,53 +1,9 @@
-var promise = new Promise (
-    //Executor
-    function (resolve, reject) {
-        // Logic
-        // Thanh cong: resole()
-        // That bai: reject()
+var fakeAPI = 'http://localhost:3000/posts';
 
-        // Fake call API
-        resolve();
-    }
-)
-
-// promise
-//     .then(function (){
-//         return new Promise(function (resolve){
-//             setTimeout(resolve, 3000);
-//         })
-//     })
-//     .then(function (data){
-//         console.log('data', data);
-//     })
-//     .then(function (data2){
-//         console.log('data2', data2);
-//         return 100
-//     })
-//     .catch(function (error){
-//         console.log(error)
-//     })
-//     .finally(function (){
-//         console.log('Done')
-//     })
-
-
-// in ra so 1 - 2 - 3 cach nhau 1s ko sd setInterval
-function sleep(ms) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, ms);
+fetch(fakeAPI)
+    .then(function (response){
+        return response.json();
     })
-}
-
-sleep(1000)
-    .then(function (){
-        console.log(1);
-        return sleep(1000);
-    })
-    .then(function (){
-        console.log(2);
-        return sleep(1000);
-    })
-    .then(function (){
-        console.log(3);
-        return sleep(1000);
+    .then(function (post){
+        console.log(post);
     })
